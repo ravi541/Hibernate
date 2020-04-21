2bc50f8a12b631d90da7246bf8cbf519c95d13aa
ravi541 <ravitj766@gmail.com>
2020-04-21 14:33:28 +0530

    hib commit

---
+++ InhertianceMapHibTablePerConcClass(XML)/src/com/ravi/entity/LedTV.java
@@ -0,0 +1,22 @@
+package com.ravi.entity;
+
+public class LedTV extends TV {
+	
+	private String tname;
+	private String tcolor;
+	
+	public String getTname() {
+		return tname;
+	}
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+	public String getTcolor() {
+		return tcolor;
+	}
+	public void setTcolor(String tcolor) {
+		this.tcolor = tcolor;
+	}
+	
+
+}
